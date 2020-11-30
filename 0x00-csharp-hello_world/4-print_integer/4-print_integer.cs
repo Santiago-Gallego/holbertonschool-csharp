3d058c4ceff53478a988c0c46fd11a04d86bff91
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-11-30 15:41:12 -0500

    [haciendolo como solo le gusta a holbi]

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -4,6 +4,6 @@ class Program
     static void Main(string[] args)
     {
             int number = 972;
-            Console.WriteLine("{0} Mission Street", number);
+            Console.WriteLine($"{number} Mission Street");
     }
 }
