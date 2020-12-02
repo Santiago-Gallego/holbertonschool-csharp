1f3046e299818b46fd33b5bff73cda3ae3b1b0c6
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-12-02 18:20:21 -0500

    [Add][14]

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -6,15 +6,10 @@ namespace _14_rectangular_array
     {
         static void Main(string[] args)
         {
-            int[,] array = new int [5,5];
-            array [2,2] = 1;
-            for (int i = 0; i < 5; i++){
-                for (int j = 0; j < 5; j++){
-                    if (j != 0)
-                        Console.Write(" ");
-                    Console.Write(array [i, j]);
-                }
-                Console.Write(" ");
+            int[,] array = new int[5, 5];
+            array[2, 2] = 1;
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("{0} {1} {2} {3} {4}", array[i, 0], array[i, 1], array[i, 2], array[i, 3], array[i, 4]);
             }
         }
     }
