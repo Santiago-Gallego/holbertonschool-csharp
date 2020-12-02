08e4af48cefe67a97166550bb55aa9533a14ac8d
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-12-02 18:07:45 -0500

    [Add][13-15]

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = int.MinValue;
+        string maxKey = "None";
+        foreach (KeyValuePair<string, int> item in myList){
+            if (item.Value >= max){
+                max = item.Value;
+                maxKey = item.Key;
+            }
+        }
+        return maxKey;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int i = 0; i < myMatrix.GetLength(0); i++) {
+            for (int j = 0; j < myMatrix.GetLength(1); j++) {
+                myMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+        return myMatrix;
+    }
+}
